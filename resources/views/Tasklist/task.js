2c8c55912780477d4974b1ff7beb5ee67fabf8bc
fabriciogon33 <fabriciogon33@gmail.com>
2021-10-29 20:35:22 -0300

    Merge https://github.com/laisriquieri/projeto-integrador-1

---
+++ resources/views/Tasklist/task.js
@@ -0,0 +1,14 @@
+var task = [];
+function idGenerator(){
+    var timestamp = new Date();
+    var id = 
+    timestamp.getHours().toString()+
+    timestamp.getMinutes().toString()+
+    timestamp.getSeconds().toString()+
+    timestamp.getMilliseconds().toString();
+return id;
+}
+
+function createTask(){
+    var taskDesscription = document.getElementById()
+}
