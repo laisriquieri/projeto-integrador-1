63da04d7ccc826978467909d6a6a0c26cfaa57b2
fabriciogon33 <fabriciogon33@gmail.com>
2021-11-07 20:36:46 -0300

    edição de relatórios

---
+++ public/scrips.js
@@ -0,0 +1,61 @@
+const masks = {
+    
+    money (value) {
+        const cleanValue = +value.replace(/\D+/g, '')
+        const options = { style: 'currency', currency: 'BRL' }
+        return new Intl.NumberFormat('pt-br', options).format(cleanValue / 100)
+      },
+    cep (value) {
+        return value
+        .replace(/\D+/g, '')
+        .replace(/(\d{5})(\d)/, '$1-$2')
+        .replace(/(-\d{3})\d+?$/, '$1')
+    },
+    cpf (value) {
+        return value
+      .replace(/\D+/g, '')
+      .replace(/(\d{3})(\d)/, '$1.$2')
+      .replace(/(\d{3})(\d)/, '$1.$2')
+      .replace(/(\d{3})(\d{1,2})/, '$1-$2')
+      .replace(/(-\d{2})\d+?$/, '$1')
+    },
+
+    cnpj (value) {
+        return value
+        .replace(/\D+/g, '')
+        .replace(/(\d{2})(\d)/, '$1.$2')
+        .replace(/(\d{3})(\d)/, '$1.$2')
+        .replace(/(\d{3})(\d)/, '$1/$2')
+        .replace(/(\d{4})(\d)/, '$1-$2')
+        .replace(/(-\d{2})\d+?$/, '$1')
+    },
+
+    phone (value) {
+        return value
+        .replace(/\D+/g, '')
+        .replace(/(\d{2})(\d)/, '($1) $2')
+        .replace(/(\d{4})(\d)/, '$1-$2')
+        .replace(/(\d{4})-(\d)(\d{4})/, '$1$2-$3')
+        .replace(/(-\d{4})\d+?$/, '$1')
+    },
+
+    phoneDDI (value) {
+        return value
+        .replace(/\D+/g, '')
+        .replace(/(\d{2})(\d)/, '+$1 $2')
+        .replace(/(\d{2})(\d)/, '($1) $2')
+        .replace(/(\d{4})(\d)/, '$1-$2')
+        .replace(/(\d{4})-(\d)(\d{4})/, '$1$2-$3')
+        .replace(/(-\d{4})\d+?$/, '$1')
+    },
+}
+
+
+document.querySelectorAll('input').forEach(($input) => {
+    const field = $input.dataset.js
+
+    $input.addEventListener('input', (e) => {
+    
+        e.target.value = masks[field](e.target.value)
+    }, false)
+})
